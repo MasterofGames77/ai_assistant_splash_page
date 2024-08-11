b119c26dc50ebc3f51b8af6d6bab9e0366751c50
MasterofGames77 <mgambardella16@gmail.com>
2024-08-11 10:35:05 -0400

    added application render file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import ErrorBoundary from './ErrorBoundary';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const clerkFrontendApi = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkFrontendApi) {
+  throw new Error("Missing Clerk Publishable Key");
+}
+
+console.log('Clerk Publishable Key:', clerkFrontendApi);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <ClerkProvider publishableKey={clerkFrontendApi}>
+        <App />
+      </ClerkProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
